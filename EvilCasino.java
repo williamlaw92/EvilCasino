f2459153eb869bb36caeb594bc3f43965b5a7de5
williamlaw92 <55024106+williamlaw92@users.noreply.github.com>
2019-12-27 20:11:56 -0500

    Update EvilCasino.java

--- EvilCasino.java
+++ EvilCasino.java
@@ -5,6 +5,16 @@ import java.io.PrintWriter;
 
 public class EvilCasino{
   public static void main (String [] args) throws IOException{
+    SlotMachine s = new SlotMachine("slot-machine.txt")'
+      Scanner scan = new Scanner (System.in);
+    
+    System.out.println("Welcome to EvilCasino, What is your name?");
+    Scanner name = new Scanner (System.in);
+    System.out.println("Please place your bet");
+    Scanner 
+    
+    String input = "";
+    
     
 
 
@@ -13,9 +23,35 @@ public class EvilCasino{
 
 
 Public class SlotMachine{
-
+  public SlotMachine(String filename)throws IOException{
+    Scanner s = new Scanner (new File(filename));
+    
+  }
+   public double pullLever ( 
   
-
+   public static int randomizer(){
+     return (int) Math.ceil(Math.random() * 3);
+   }
+     
+    
+    
+    private char smileyFace = \u263A;
+    private char heart = \u2764;
+    private char seven = \u0037;
+    private char three = \u0033;
+    private char frownFace = \u2639;
+    private char diamond = \u2666;
+    private char ex = \u2743;
+    private char copyright = \u00A9;
+    private char check = \u2713;
+    private char dollar = \u0024;
+    private char slot1;
+    private char slot2;
+    private char slot3;
+    private double betAmount;
+    
+    
+    
 
 
 Public class bank{
